c0dbe75121e51aeb35822d05ab61e785343737b5
acoross <acoross@gmail.com>
2016-09-11 15:29:20 +0900

    d

--- PacketSerializer/PacketSerializer/main.cpp
+++ PacketSerializer/PacketSerializer/main.cpp
@@ -16,17 +16,9 @@ using namespace packet_serializer_1;
 
 class MyPacket : public Packet {
 public:
-	MyPacket()
-	: i_val_(this)
-	, c_val_(this), ba_val_(this), str_val_(this)
-	{
-		
-	}
-
-	IntType i_val_;
-	CharType c_val_;
-	StringType str_val_;
-	ByteArrayType<10> ba_val_;
+	IntType i_val_{ this };
+	CharType c_val_{ this };
+	StringType str_val_{ this };
 };
 
 
@@ -35,7 +27,7 @@ int main(int argc, const char * argv[]) {
 	packet.i_val_.data_ = 0xcdcdffab;
 	packet.c_val_.data_ = 'c';
 	packet.str_val_.data_ = std::string("bakashinji");
-
+	
 	unsigned char buffer[100]{0,};
 	packet.Serialize(buffer);
 
